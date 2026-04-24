4ffe47e33c40d55997b499a99c62eb23e37a554b
petr <petyalom@yandex.ru>
2026-04-24 11:58:18 +0300

    fixes

--- src/main/kotlin/ru/ravel/ultunneladminpanel/model/config/ConfigDataNaive.kt
+++ src/main/kotlin/ru/ravel/ultunneladminpanel/model/config/ConfigDataNaive.kt
@@ -3,15 +3,11 @@ package ru.ravel.ultunneladminpanel.model.config
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
 import org.hibernate.annotations.JdbcTypeCode
 import org.hibernate.type.SqlTypes
-import ru.ravel.ultunneladminpanel.model.Proxy
 
 @Entity
 data class ConfigDataNaive(
@@ -38,7 +34,7 @@ data class ConfigDataNaive(
 	var tls: TlsSettings? = null,
 
 	@JsonProperty("domain_resolver")
-	var domainResolver: String? = "cloudflare",
+	var domainResolver: String? = null,
 
 	@JdbcTypeCode(SqlTypes.JSON)
 	@JsonProperty("udp_over_tcp")
